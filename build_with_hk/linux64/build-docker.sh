c0c3b894ffe3be264db3ac72ccfea25d2f467c3a
zy@ondev <284141050@qq.com>
2021-06-03 19:54:52 +0800

    feat(all): add stdout

--- build_with_hk/linux64/build-docker.sh
+++ build_with_hk/linux64/build-docker.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
+echo "apt update ......"
 apt update
 
+echo "install unzip ......"
 apt install -y unzip 
 
+echo "get hksdk ......"
 wget https://github.com/paratek-custom/assets/raw/main/hksdk/CH-HCNetSDKV6.1.6.45_build20210302_linux64_20210428163105.zip -O hksdk.zip
-
 unzip hksdk.zip
 
 mkdir ${LD_LIBRARY_PATH} -p
-
 mv CH-HCNetSDKV6.1.6.45_build20210302_linux64/lib/* ${LD_LIBRARY_PATH}
 
 ## go tool
+echo "get go tools ......"
+echo "get go tools minify ..."
 go get github.com/tdewolff/minify/cmd/minify
+echo "get go tools go-bindata ..."
 go get github.com/go-bindata/go-bindata/go-bindata
+echo "get go tools go-bindata-assetfs ..."
 go get github.com/elazarl/go-bindata-assetfs/go-bindata-assetfs
 
 
 ## clear 。。。。。。
+echo "clear caches"
 apt remove -y unzip
 apt autoremove -y
 apt clean
